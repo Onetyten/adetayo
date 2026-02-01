f4743e71d511499fd6649ccd65eeea242da13655
onetyten <labaekaabdulrazaq@gmail.com>
2026-02-01 23:25:26 +0100

    typescript

---
+++ src/Pages/Intro/About.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import aboutImage from "../../assets/profile.webp"
+import ConsoleText from "../../ConsoleText"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+
+export default function About() {
+
+    const [hoveringGit,setHoveringGit] = useState(false)
+
+  return (
+    <div className='flex flex-col flex-grow justify-center gap-12 h-full w-full'>
+        <div className="w-full flex justify-center items-center">
+            <img src={aboutImage} alt="" className="w-56 hover:shadow-[0px_0px_0px_#292824] transition-all duration-500 shadow-[0px_0px_0px_#292824] rounded-full object-contain" />
+        </div> 
+
+        <ConsoleText projectname = 'About' className="w-full">
+            <div className='flex flex-col text-base gap-3 sm:gap-4'>
+                <div>
+                    <p className='text-lg'>Labaeka_Adetayo</p>
+                </div>
+                <div>
+                    <p className='text-muted' >Stack : <span className='text-text'>MERN_STACK</span></p> 
+                </div>
+
+                <div>
+                    Hi, i&apos;m Adetayo a Fullstack developer with a lifelong obsession for building. Over the years i&apos;ve worked on a diverse range of projects from high performance real-time backend systems to high fidelity frontends.
+                </div>
+
+                <a href= "https://github.com/Onetyten" target='_blank' onMouseEnter={()=>setHoveringGit(prev=>(prev?prev:true))} onMouseLeave={()=>setHoveringGit(prev=>(prev?false:prev))} className="border-1 text-text hover:text-darkgrey w-fit cursor-pointer border-muted p-[1px] bg-card flex relative items-center transition-all duration-300 h-12">
+                    <div className={`h-11 absolute bg-text aspect-square flex ${hoveringGit?"justify-start w-full":"w-11"} pl-2.5 transition-all duration-300 items-center`}>
+                        <FontAwesomeIcon icon={faGithub} className="h-full x-0 bg-text text-darkgrey text-xl" />
+                    </div>
+                    <span className="px-6 pl-14 text-sm font-bold uppercase relative z-10">Github</span> 
+                </a>
+
+            </div>
+        </ConsoleText>
+    </div>
+  )
+}
---
+++ src/Pages/Intro/ConnectCard.tsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { useRef } from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+
+export default function ConnectCard() {
+    const iconRef = useRef<(SVGSVGElement | null)[]>([])
+    useGSAP(()=>{
+        gsap.from (iconRef.current,{x:'-200', opacity:0, stagger:0.15,duration:1,delay:0.5})
+    })
+
+    const links = [
+        {icon:faGithub,url:"https://github.com/Onetyten"},
+        {icon:faLinkedin,url:"https://www.linkedin.com/in/labaeka-adetayo-a9681a247"},
+        {icon:faEnvelope,url:"mailto:labaekaabdulrazaq@gmail.com"},
+        {icon:faPhone,url:"tel:+2349155073769"},
+        {icon:faTwitter,url:"https://x.com/Onetyten_exe"}
+    ]
+
+  return (
+    <div className='w-full h-full flex items-center justify-center gap-6'>
+        <div>
+            You can reach me on
+        </div>
+        <div className="flex gap-6 items-center overflow-hidden">
+            {links.map((item,index)=><a  key={index}  href={item.url} target='_blank' rel="noopener noreferrer">
+                <FontAwesomeIcon icon={item.icon} className="hover:text-text text-2xl" ref ={(el)=>{iconRef.current[index] = el}}/>
+            </a>)}
+        </div>
+    </div>
+  )
+}
